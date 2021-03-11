a94e16c4af217cca698b5f70d5b40d32f99a09f5
Matthew Mawdsley <matthew@mawdsley.me>
2021-03-11 09:17:39 +0000

    Swap control and alt back around.  Add delete key.

--- keymap.c
+++ keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS, \
   CTL_T(KC_ESC),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_HOME,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-           KC_LGUI,  KC_LALT,KC_LCTRL, KC_LOWER,  KC_ENT,            KC_SPC,  KC_RAISE, KC_RCTRL,  KC_RALT, KC_RGUI \
+           KC_LGUI,  KC_LCTRL,KC_LALT, KC_LOWER,  KC_ENT,            KC_SPC,  KC_RAISE, KC_RALT,  KC_RCTRL, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(                                                 \
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_DEL,\
   _______,    KC_PGUP,    KC_UP,    KC_PGDN,    KC_HOME,    _______,         _______,  KC_LBRC, KC_RBRC, _______, _______, _______,                                                                     \
   _______, KC_LEFT,   KC_DOWN, KC_RGHT,  _______, _______,                       _______, KC_LPRN, KC_RPRN, KC_EQL, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______,       _______, _______, KC_MINS, KC_LABK, KC_RABK, _______, _______, \
