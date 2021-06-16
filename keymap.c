d7a68ceff1e105bc77a4f9932247db244564df89
Matthew Mawdsley <matthew@mawdsley.me>
2021-06-16 17:07:15 +0100

    Replace right GUI with delete

--- keymap.c
+++ keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LCTR | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RAlt | RCTR | RGUI |
+ *            | LGUI | LCTR | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RAlt | RCTR | Del  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,  KC_U,  KC_I,   KC_O,   KC_P,   KC_BSLS,
   CTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,  KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
   KC_LSFT,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     _______,KC_N,  KC_M,  KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
-                  KC_LGUI,  KC_LCTRL,KC_LALT,MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), KC_RALT,  KC_RCTRL, KC_RGUI
+                  KC_LGUI,  KC_LCTRL,KC_LALT,MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), KC_RALT,  KC_RCTRL, KC_DEL
 ),
 /*
  * QWERTY
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LCTR | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RAlt | RCTR | RGUI |
+ *            | LGUI | LCTR | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RAlt | RCTR | Del  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,  KC_U,  KC_I,   KC_O,   KC_P,   KC_BSLS,
   CTL_T(KC_ESC),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,  KC_J,  KC_K,   KC_L,   KC_SCLN,KC_QUOT,
   KC_LSFT,      KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     _______,KC_N,  KC_M,  KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
-                  KC_LGUI,  KC_LCTRL,KC_LALT,MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), KC_RALT,  KC_RCTRL, KC_RGUI
+                  KC_LGUI,  KC_LCTRL,KC_LALT,MO(_LOWER), KC_ENT,      KC_SPC,  MO(_RAISE), KC_RALT,  KC_RCTRL, KC_DEL
 ),
 /*
  * QWERTY (gaming)
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |      | RClk | MClk | LClk |WheelD|-------|    |-------|  _   |  -   |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | Del  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |  F9  |  F10 |  F11 |  F12 |      |-------|    |-------| Home | PgUp | PgDn | End  |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | Del |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -132,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |      |WheelL|      |WheelR|WheelD|-------|    |-------|      | PREV | PLAY | NEXT |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | Del |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -153,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | Del  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
